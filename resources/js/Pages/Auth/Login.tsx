18df7619d2a6b213347ca0fa1240767f99ee67c3
quenio roger <queniorojer_rm@hotmail.com>
2023-12-24 15:34:14 -0300

    update

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -85,22 +85,16 @@ export default function Login({ status, canResetPassword }: { status?: string, c
                         >
                             Forgot your password?
                         </Link>
-                    )}
+                    )
 
-                        <Link style={{left:"-100px", position:"relative"}}
-                            href={route('register')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Register
-                        </Link>
+                    }
 
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-
+                    <Link style={{position:"relative", left:"-40px"}}
+                        href={route('register')}
+                        className="ms-4"
+                    >
+                        Register
+                    </Link>
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
                     </PrimaryButton>
