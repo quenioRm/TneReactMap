ecd63c6c36cdc4600790f4c03dd8ff0940710a18
quenio roger <queniorojer_rm@hotmail.com>
2024-01-13 14:33:40 -0400

    update

--- database/migrations/2024_01_13_070425_personal_marker_activity.php
+++ database/migrations/2024_01_13_070425_personal_marker_activity.php
@@ -13,7 +13,7 @@ return new class extends Migration
      {
          Schema::create('personal_marker_activity', function (Blueprint $table) {
              $table->id();
-             $table->unsignedBigInteger('personalMarkerId');
+             $table->integer('personalMarkerId');
              $table->string('unity')->nullable();
              $table->decimal('previouscount', 5, 2)->default(0);
              $table->decimal('lenPercent', 5, 2)->default(0);
