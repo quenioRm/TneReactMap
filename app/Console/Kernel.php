ecf4a2a6cb744098f20226266127eab6ef6c0a95
quenio roger <queniorojer_rm@hotmail.com>
2024-01-21 14:41:28 -0400

    fixed cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Http\Controllers\MapsController;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class Kernel extends ConsoleKernel
 {
@@ -20,22 +22,24 @@ class Kernel extends ConsoleKernel
             // Check if the cache value exists
             if ($cacheValue === null) {
                 // The cache key doesn't exist, so execute the function from the controller
-                app()->call([MapsController::class, 'getCoordinates']);
+                $mapsController = new MapsController();
+                $mapsController->getCoordinates();
             } else {
                 // Calculate the remaining time until expiration
                 $now = now()->timestamp;
-                $expirationTimestamp = $cacheValue['expires_at'];
+                $expirationTimestamp = $cacheValue['expiration'];
                 $ttl = $expirationTimestamp - $now;
 
                 // If the remaining time is less than or equal to zero, execute the function from the controller
                 if ($ttl <= 0) {
-                    app()->call([MapsController::class, 'getCoordinates']);
+                    $mapsController = new MapsController();
+                    $mapsController->getCoordinates();
                 } else {
                     // Log a message indicating that the function wasn't executed due to valid cache
                     Log::info('The MapsController::getCoordinates function was not executed due to a valid cache.');
                 }
             }
-        })->twiceDaily(0, 12);
+        })->everyMinute(30);
     }
 
     /**
