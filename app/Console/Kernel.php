d283081e0222f6175ea30469af4953ec4b1e6815
quenio roger <queniorojer_rm@hotmail.com>
2024-01-21 20:58:41 -0400

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Http\Controllers\MapsController;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,24 +15,31 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
-
-        // Exec Maps
         $schedule->call(function () {
-            // Verifique se a chave do cache existe
-            if (!Cache::has('coordinates_data_general')) {
-                // A chave do cache não existe ou expirou, então execute a função do controller
-                app()->call([MapsController::class, 'getCoordinates']);
-            } else {
-                // A chave do cache existe, verifique o tempo restante de vida
-                $ttl = Cache::getTtl('coordinates_data_general');
+            // Retrieve the cache value
+            $cacheValue = Cache::get('coordinates_data_general');
 
-                // Se o tempo restante de vida for menor ou igual a zero, execute a função do controller
+            // Check if the cache value exists
+            if ($cacheValue === null) {
+                // The cache key doesn't exist, so execute the function from the controller
+                $mapsController = new MapsController();
+                $mapsController->getCoordinates();
+            } else {
+                // Calculate the remaining time until expiration
+                $now = now()->timestamp;
+                $expirationTimestamp = $cacheValue['expiration'];
+                $ttl = $expirationTimestamp - $now;
+
+                // If the remaining time is less than or equal to zero, execute the function from the controller
                 if ($ttl <= 0) {
-                    app()->call([MapsController::class, 'getCoordinates']);
+                    $mapsController = new MapsController();
+                    $mapsController->getCoordinates();
+                } else {
+                    // Log a message indicating that the function wasn't executed due to valid cache
+                    Log::info('The MapsController::getCoordinates function was not executed due to a valid cache.');
                 }
             }
-        })->twiceDaily(0, 12);
+        })->everyMinute(30);
     }
 
     /**
