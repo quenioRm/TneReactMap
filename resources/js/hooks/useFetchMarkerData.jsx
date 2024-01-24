9459a3ce3e38722379164b0b8da3ea201dee0591
quenio roger <queniorojer_rm@hotmail.com>
2024-01-24 06:58:17 -0400

    fix

--- resources/js/hooks/useFetchMarkerData.jsx
+++ resources/js/hooks/useFetchMarkerData.jsx
@@ -1,16 +1,14 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "../Components/axiosInstance";
 
-/**
- * Custom hook to fetch marker data from an API and retry on failure every 3 seconds.
- */
 const useFetchMarkerData = () => {
     const [markerData, setMarkerData] = useState([]);
+    const [isDataFetchedSuccessfully, setIsDataFetchedSuccessfully] = useState(false);
+    const [error, setError] = useState(null);
     const [latestCalledCoordinate, setLastestCalledCoordinate] = useState({});
     const [currentCalledLatLng, setCurrentCalledLatLng] = useState({});
     const [firstCalledLatLng, setFirstCalledLatLng] = useState({});
     const [latestCalledLatLng, setLastestCalledLatLng] = useState({});
-    const [error, setError] = useState(null);
     const [mapConfig, setMapConfig] = useState({
         center: { lat: 0, lng: 0 },
         zoom: 15,
@@ -18,27 +16,17 @@ const useFetchMarkerData = () => {
 
     const retryTimeoutRef = useRef(null);
 
-    const fetchAllMarkerData = async () => {
-        try {
-            if (retryTimeoutRef.current) {
-                clearTimeout(retryTimeoutRef.current);
-            }
+    const mapType = localStorage.getItem("mapType") || 'hybrid';
 
+    const fetchAllMarkerData = async () => {
+        if (isDataFetchedSuccessfully) {
+            // Se os dados já foram buscados com sucesso, não faça nada.
+            return;
+        }
+
+        try {
             const response = await axios.get("/api/get-coordinates");
             const latestItem = response.data[response.data.length - 1];
-
-            setMapConfig({
-                center: {
-                    lat: response.data[0].position.lat,
-                    lng: response.data[0].position.lng,
-                },
-                zoom: 15,
-                gestureHandling: "greedy",
-                mapTypeId:
-                    mapType === "hybrid"
-                        ? google.maps.MapTypeId.SATELLITE
-                        : google.maps.MapTypeId.ROADMAP,
-            });
 
             setLastestCalledCoordinate({
                 x: parseFloat(latestItem.position.utmx),
@@ -75,18 +63,19 @@ const useFetchMarkerData = () => {
             });
 
             setMarkerData(response.data);
-            clearTimeout(retryTimeoutRef.current);
+            setIsDataFetchedSuccessfully(true);  // Indica sucesso na requisição
+            setError(null);  // Limpa qualquer erro anterior
         } catch (err) {
             setError(err);
             console.error("Error fetching marker data:", err);
-
+            // Tenta novamente após 3 segundos se a requisição anterior falhou
             retryTimeoutRef.current = setTimeout(fetchAllMarkerData, 3000);
         }
     };
 
     useEffect(() => {
         fetchAllMarkerData();
-
+        // Função de limpeza para cancelar a re-tentativa se o componente for desmontado
         return () => {
             if (retryTimeoutRef.current) {
                 clearTimeout(retryTimeoutRef.current);
